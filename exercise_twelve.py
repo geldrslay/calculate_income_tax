15bcdd56d4c05753bb31027f93b2bf3bb43bc309
Angela Ferrer <gelferrero01@gmail.com>
2024-01-26 05:30:39 +0800

    display given

---
+++ exercise_twelve.py
@@ -0,0 +1,9 @@
+# Calculate income tax for the given income by adhering to the rules below
+# First $10,000 has 0% rate
+# Next $10,000 has a 10% rate
+# The remaining amount has a 20% rate. 
+
+# Display the given income. 
+given_income = 154000
+print ('The given income is: $', given_income)
+
