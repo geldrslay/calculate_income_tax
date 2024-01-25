4e792a000c59be13052521aee7f60fc4578e4a95
Angela Ferrer <gelferrero01@gmail.com>
2024-01-26 05:52:01 +0800

    Determine tax payable amount

--- exercise_twelve.py
+++ exercise_twelve.py
@@ -4,6 +4,19 @@
 # The remaining amount has a 20% rate. 
 
 # Display the given income. 
-given_income = 154000
+given_income = 15000
 print ('The given income is: $', given_income)
+
+# Determine income tax payable if income is less than or equal to $10,000. 
+if given_income <= 10000:
+    tax_payable = 0
+# Determine income tax payable if income is less than or equal to $20,000
+elif given_income <= 20000:
+    income_with_tax= given_income - 10000
+    tax_payable = income_with_tax * 10 / 100
+# Determine income tax payable if income is greater than $20,000
+else: 
+    tax_payable = 0
+    tax_payable = 10000 * 10 /100
+    tax_payable += (given_income-20000) * 20 / 100
 
